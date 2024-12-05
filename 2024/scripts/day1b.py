89ba22c4d2244eed31984991c7ca21188def1354
Pip Jackson <pipbjackson@icloud.com>
2024-12-05 21:12:21 +0000

    1b

--- 2024/scripts/day1b.py
+++ 2024/scripts/day1b.py
@@ -1,1 +1,22 @@
-import 1a
+import numpy as np
+from day1a import c, d
+
+def similarity_score(a, b):
+    d = dict(zip(*np.unique(b, return_counts=True)))
+    def f(x):
+        if x in d.keys():
+            return x * d[x]
+        else:
+            return 0
+    return np.vectorize(f)(a)
+
+# print(
+#     list(zip(c, similarity_score(c, d)))
+#     )
+
+ans = np.sum(similarity_score(c, d))
+
+if __name__ == "__main__":
+    print(ans)
+    # print()
+    pass
